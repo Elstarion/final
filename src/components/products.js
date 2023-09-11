0bd9b9739d13541dccbed418b00960655521aa81
Elstarion <vladlapitsky37@gmail.com>
2023-09-11 06:26:55 +0300

    final

--- src/components/products.js
+++ src/components/products.js
@@ -1,80 +1,94 @@
+import image1 from "../assets/1.png";
+import image2 from "../assets/2.png";
+import image3 from "../assets/3.png";
+import image4 from "../assets/4.png";
+import image5 from "../assets/5.png";
+import image6 from "../assets/6.png";
+import image7 from "../assets/7.png";
+import image8 from "../assets/8.png";
+import image9 from "../assets/9.png";
+import image10 from "../assets/10.png";
+import image11 from "../assets/11.png";
+import image12 from "../assets/12.png";
+import image13 from "../assets/13.png";
+
 const products = [
   {
     id: 1,
     name: "Apple iPhone 14 Pro Max",
-    image: "assets/1.png",
+    image: image1,
     price: 1625.0,
   },
   {
     id: 2,
     name: "Samsung Galaxy S23 Ultra",
-    image: "assets/2.png",
+    image: image2,
     price: 1511.0,
   },
   {
     id: 3,
     name: "Apple MacBook Pro 16'' M2",
-    image: "assets/3.png",
+    image: image3,
     price: 3371.0,
   },
   {
     id: 4,
     name: "MSI Pulse",
-    image: "assets/4.png",
+    image: image4,
     price: 2545.88,
   },
   {
     id: 5,
     name: "Super Size 50 Gallon Cowboy Hats",
-    image: "assets/5.png",
+    image: image5,
     price: 21.02,
   },
   {
     id: 6,
     name: "Skechers Shoes",
-    image: "assets/6.png",
+    image: image6,
     price: 88.55,
   },
   {
     id: 7,
     name: "Yamato Sword",
-    image: "/src/assets/7.png",
+    image: image7,
     price: 25000,
   },
   {
     id: 8,
     name: "Just Learn",
-    image: "assets/8.png",
+    image: image8,
     price: 39.99,
   },
   {
     id: 9,
     name: "Outroad Fat Tire",
-    image: "assets/9.png",
+    image: image9,
     price: 219.99,
   },
   {
     id: 10,
     name: "There's No Place Like Home Sign",
-    image: "assets/10.png",
+    image: image10,
     price: 540.0,
   },
   {
     id: 11,
     name: "Multi -Screen Encoding Cabin Workstation",
-    image: "assets/11.png",
+    image: image11,
     price: 11099.0,
   },
   {
     id: 12,
     name: "Paul Stanley Replica Miniature Guitar",
-    image: "assets/12.png",
+    image: image12,
     price: 18.95,
   },
   {
     id: 13,
     name: "Papyrus Blank Thank You Card",
-    image: "assets/13.png",
+    image: image13,
     price: 10.99,
   },
 ];
