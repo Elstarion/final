fc625fbac9a1a8cd1370e538b5801984ec086d01
Elstarion <vladlapitsky37@gmail.com>
2023-09-11 05:41:57 +0300

    final

--- src/components/shop/Cart.jsx
+++ src/components/shop/Cart.jsx
@@ -11,8 +11,7 @@ import {
   removeFromCart,
 } from "../../store/cartSlice";
 import { useEffect } from "react";
-
-// import sfsf from "../../assets/jusdoit.png";
+import justdoit from "/src/assets/justdoit.png";
 
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
@@ -52,7 +51,7 @@ const Cart = () => {
                 <span>Start shopping</span>
               </Link>
             </div>
-            <img src="/justdoit.png" alt="justdoit" className={css.justdoit} />
+            <img src={justdoit} alt="justdoit" className={css.justdoit} />
           </div>
         ) : (
           <div>
@@ -67,7 +66,7 @@ const Cart = () => {
               {cart.cartItems?.map((item) => (
                 <div className={css.cart_item} key={item.id}>
                   <div className={css.cart_product}>
-                    <img src={`/final/shop/${item.image}`} alt={item.name} />
+                    <img src={item.image} alt={item.name} />
                     <div className={css.item_description}>
                       <h3>{item.name}</h3>
                       <button onClick={() => handleRemoveFromCart(item)}>
